70d7488c08d95ea35a6dc247f35a056577f834f1
gitcoder-23 <prabhatece21@gmail.com>
2021-07-03 14:59:11 +0530

    🎉

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,12 +4,7 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark py-2">
-           <Link to='/' className="navbar-brand ml-5">Redux Contact App</Link>
-           <ul className="navbar-nav">
-            <li className="nav-item">
-                <Link to='/' className="navbar-brand ml-5">Redux Contact App</Link>
-            </li>
-           </ul>
+           <Link to='/' className="navbar-brand ml-5">Redux Contact App - Developed by Prabhat</Link>
         </nav>
     )
 }
